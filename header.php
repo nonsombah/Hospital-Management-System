2a9dbd0a37c3ee8180c789921225cadb9d13289e
nonso mbah <nonsombah99@gmail.com>
2015-11-04 02:22:54 +0100

    Create header.php
    the header fie

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Salem Hospital Management System</title>
+	
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />	
+	</head>
+	<body style="background-color: powderblue">
+	
+
+	<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+		<h1 class="page-header" ><img id="logo" src="images/hospital.png" alt="Salem hospital" height= "140" width="200" title = "Hospital Management System" />
+		<a href='logout.php?username' style="text-decoration:none" class="text text-danger bg-success">Salem Hospital Management System</a></h1>
+		</div>
+	</div>
