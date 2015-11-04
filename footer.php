9ae670dda7a25d0250bfa78eb4f4990c7731c510
nonso mbah <nonsombah99@gmail.com>
2015-11-04 02:24:04 +0100

    Create footer.php
    footer file

---
+++ footer.php
@@ -0,0 +1,31 @@
+</div>
+ <?php
+	
+$year = date('Y');
+  
+ ?>
+
+ <div class ="footer">
+ <div class="container-fluid">
+ <p class='navbar-text'>copyright <?php echo $year ?> &copy; Hospital management system, a production of Mbah Johnson Favour. 
+ 
+
+research project submitted to the department of computer science school of industrial and applied science federal polytechnic nekede owerri
+
+in partial fulfillment for the requirement for the award of national diploma (nd) in computer science.<br />
+All Rights reserved, this system is open source and subject to modifications and enhancements and should not be distributed for profits. <a href ="manual.html"
+class="navbar-btn btn btn-info">
+User Manual</a>
+
+
+ </p>
+ </div>
+ </div>
+
+
+ 
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+
+ </body>
+</html>
